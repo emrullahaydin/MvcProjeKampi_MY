da631679f950f1ad73eb530849b9dbfeaa2e6c5a
Emrullah AYDIN <61662213+emrullahaydin@users.noreply.github.com>
2021-05-12 13:06:06 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Content.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+   public class Content
+    {
+        [Key]
+        public int ContentID { get; set; }
+        [StringLength(5100)]
+        public string ContentValue { get; set; }
+        public DateTime ContentDate { get; set; }
+        
+        public int HeadingID { get; set; }
+        public virtual Heading Heading { get; set; } // Birebir ilişki kuruluyor. Üst satır ilişkili tablodaki alanın aynısı olmak zorunda.
+
+        public int WriterID { get; set; }
+        public virtual Writer Writer { get; set; }
+    }
+}
